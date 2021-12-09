6f890d41de8f2dcc1fafa066983b6e7ab0c93b67
Fabian Gottesmann <77332349+fgottesmann@users.noreply.github.com>
2021-12-09 13:59:28 +0100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import path from 'path';
+import express from 'express';
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+// Serve storybook production bundle
+app.use('/storybook', express.static('dist/storybook'));
+
+// Serve app production bundle
+app.use(express.static('dist/app'));
+
+// Handle client routing, return all requests to the app
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, 'app/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
+});
