15bc4f61d6a3ae7cc5b7cfe39bb197f5ba1e60c3
fgottes <f.gottesmann@outlook.com>
2021-12-10 16:23:05 +0100

    ✨ Add zip code button and associated story

---
+++ src/app/components/Input/Zipcode.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import PLZInput from './Zipcode';
+
+export default {
+  title: 'Component/PLZInput',
+  component: PLZInput,
+};
+
+export const PLZ = (): JSX.Element => <PLZInput></PLZInput>;
---
+++ src/app/components/Input/Zipcode.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from './Zipcode.module.css';
+
+export default function PLZInput(): JSX.Element {
+  return (
+    <div className={styles.div}>
+      <input
+        className={styles.PLZInput}
+        placeholder="z.B. 21335"
+        type="text"
+      ></input>
+      <button className={styles.button}>Suchen</button>
+      <img
+        src="/arrowRight.svg
+      "
+        alt=""
+      />
+    </div>
+  );
+}
