60db9d77e34291732a4f17ebbacc34c4e0c7bf0c
fgottes <f.gottesmann@outlook.com>
2021-12-12 23:38:45 +0100

    🚧

---
+++ src/app/components/ColExpMulti/ColExpMulti.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ColExpMulti from './ColExpMulti';
+
+export default {
+  title: 'Component/Multi',
+  component: ColExpMulti,
+};
+
+export const Multi = (): JSX.Element => <ColExpMulti children={'HelloWorld'} />;
